fa784a85790462012e67014e2f7f0258378ab74c
AlessandraRomualdo <romualdo.alessandra23@gmail.com>
2024-04-02 18:52:37 -0300

    add layout componet filter

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -2,14 +2,36 @@ import { NgModule } from "@angular/core";
 
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
   imports: [
     MatListModule,
     MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    MatButtonModule,
   ],
   exports: [
     MatListModule,
     MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    MatButtonModule,
+  ],
+  providers: [
+    provideNativeDateAdapter()
   ],
 })
 export class AngularMaterialModule {
