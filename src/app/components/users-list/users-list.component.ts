df74cf784fb38d53179d04e1c0db221fd2bb7778
AlessandraRomualdo <romualdo.alessandra23@gmail.com>
2024-04-03 11:33:01 -0300

    add event click in table and add inputs

--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter, Input } from '@angular/core';
 import { IUser } from '../../interfaces/user/user.interface';
-import { UsersList } from '../../data/users-list';
 
 @Component({
   selector: 'app-users-list',
@@ -8,10 +7,14 @@ import { UsersList } from '../../data/users-list';
   styleUrl: './users-list.component.scss'
 })
 export class UsersListComponent {
-  usersList: IUser[] = UsersList;
+  @Input({ required: true }) usersList: IUser[] = [];
   displayedColumns: string[] = ['name', 'date', 'status'];
+
+  @Output('userSelected') userSelectedEmitt = new EventEmitter<IUser>();
 
   onUserSelected(user: IUser) {
     console.log(user);
+
+    this.userSelectedEmitt.emit(user);
   }
 }
